4ce079ca10753262524ecfa2deea0ae15bad169a
nt54hamnghi <hamnghi250699@gmail.com>
2022-03-27 19:12:37 -0400

    move data folder into resrc

--- src/bluebook-bulldozer/dirs.py
+++ src/bluebook-bulldozer/dirs.py
@@ -2,7 +2,7 @@ import pathlib
 
 ROOT_DIR = pathlib.Path(__file__).parents[2]
 CONTENT_DIR = ROOT_DIR / "src/bluebook-bulldozer/stages/content"
-DATA_DIR = ROOT_DIR / "data"
+DATA_DIR = ROOT_DIR / "resrc/data"
 LOG_DIR = ROOT_DIR / "log"
 
-print(CONTENT_DIR)
+print(ROOT_DIR)
